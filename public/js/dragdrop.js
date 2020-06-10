e969c0d01dc5a4527090fca38bc0f643d8b98912
philirol <philirol@hotmail.com>
2020-06-10 09:41:38 +0200

    comme back congif files norm

---
+++ public/js/dragdrop.js
@@ -0,0 +1,38 @@
+
+      $(function () {
+        $("#table").DataTable();
+
+        $( "#tablecontents" ).sortable({
+          items: "tr",
+          cursor: 'move',
+          opacity: 0.6,
+          update: function() {
+              sendOrderToServer();
+          }
+        });
+
+        function sendOrderToServer() {
+          var order = [];
+          var token = $('meta[name="csrf-token"]').attr('content');
+          $('tr.row1').each(function(index,element) {
+            order.push({
+              id: $(this).attr('data-id'),
+              position: index+1
+            });
+          });
+
+          $.ajax({             
+            dataType: "json", 
+            url: "{{ route('tri') }}",
+            type: 'POST',
+            data: {order: order, _token: token},
+            success: function(response) {
+                if (response.status == "success") {
+                  console.log(response);
+                } else {
+                  console.log(response);
+                }
+            }
+          });
+        }
+      });
